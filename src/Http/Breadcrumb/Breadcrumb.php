35aebbcab6c41f612ad830f03b6366a2ec71261a
mountstone <lukasz.cirut@gmail.com>
2017-11-14 14:34:17 +0100

    ADD: backend integration with new frontend

--- src/Http/Breadcrumb/Breadcrumb.php
+++ src/Http/Breadcrumb/Breadcrumb.php
@@ -11,20 +11,16 @@
  * bundled with this package in the LICENSE file.
  *
  * @package    Tester
- * @version    0.9.0
+ * @version    0.9.2
  * @author     Antares Team
  * @license    BSD License (3-clause)
  * @copyright  (c) 2017, Antares
  * @link       http://antaresproject.io
  */
 
-
-
 namespace Antares\Tester\Http\Breadcrumb;
 
 use DaveJamesMiller\Breadcrumbs\Facade as Breadcrumbs;
-use function handles;
-use function view;
 
 class Breadcrumb
 {
